1ff41273f9cadf81702bebf4fc86fd080c24f0f4
Markus Marcinek <markus.marcinek@outlook.de>
2020-10-29 21:15:57 +0100

    changed type of get ParameterID Map

--- helpers.go
+++ helpers.go
@@ -6,6 +6,7 @@ import (
 	ibeam_core "github.com/SKAARHOJ/ibeam-core-go/ibeam-core"
 )
 
+// GenerateOptionList ...
 func GenerateOptionList(options []string) (optionList *ibeam_core.OptionList) {
 	optionList = &ibeam_core.OptionList{}
 	for index, option := range options {
@@ -17,7 +18,8 @@ func GenerateOptionList(options []string) (optionList *ibeam_core.OptionList) {
 	return optionList
 }
 
-func GetNameOfParameter(parameterID uint32, pds map[string]ibeam_core.ParameterDetail) (string, error) {
+// GetNameOfParameter returns the Name of a Parameter with a given ParameterID in a given ParameterDetail Map
+func GetNameOfParameter(parameterID uint32, pds map[string]*ibeam_core.ParameterDetail) (string, error) {
 	for _, pd := range pds {
 		if pd.Id.Parameter == parameterID {
 			return pd.Name, nil
