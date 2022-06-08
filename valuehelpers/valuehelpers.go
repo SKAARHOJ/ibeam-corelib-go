d3808ac5af6cc0a50ee43753a21fab724b424d2a
Jens Lindahl <jensctl@gmail.com>
2022-06-08 13:19:24 +0200

    add support for int

--- valuehelpers/valuehelpers.go
+++ valuehelpers/valuehelpers.go
@@ -3,16 +3,23 @@ package valuehelpers
 /*
 Normalise a value from one range (minVal:maxVal) to fit within a new range (newMin:newMax)
 */
-func Normalise[T ~float32 | ~float64](val, minVal, maxVal, newMin, newMax T) T {
+func Normalise[T ~int | ~float32 | ~float64](val, minVal, maxVal, newMin, newMax T) T {
 	x := val - minVal
-	y := (newMax - newMin) / (maxVal - minVal)
-	return newMin + x*y
+	newRange := newMax - newMin
+	oldRange := maxVal - minVal
+	if newRange > oldRange {
+		y := newRange / oldRange
+		return newMin + x*y
+	} else {
+		y := oldRange / newRange
+		return newMin + x/y
+	}
 }
 
 /*
 Constrain a value between a lower (min) and upper (max) limit
 */
-func Constrain[T ~float32 | ~float64](val, min, max T) T {
+func Constrain[T ~int | ~float32 | ~float64](val, min, max T) T {
 	if val < min {
 		return min
 	}
--- valuehelpers/valuehelpers_test.go
+++ valuehelpers/valuehelpers_test.go
@@ -2,7 +2,7 @@ package valuehelpers
 
 import "testing"
 
-func TestNormalise(t *testing.T) {
+func TestNormaliseFloat(t *testing.T) {
 	type TestCase struct {
 		in       float32
 		minVal   float32
@@ -26,6 +26,32 @@ func TestNormalise(t *testing.T) {
 	for _, tc := range TestCases {
 		if got := Normalise(tc.in, tc.minVal, tc.maxVal, tc.newMin, tc.newMax); got != tc.expected {
 			t.Errorf("Got %f, but expected %f", got, tc.expected)
+		}
+	}
+}
+func TestNormaliseInt(t *testing.T) {
+	type TestCase struct {
+		in       int
+		minVal   int
+		maxVal   int
+		newMin   int
+		newMax   int
+		expected int
+	}
+
+	TestCases := []TestCase{
+		{0, 0, 10, 0, 5, 0},
+		{0, 0, 10, 0, 20, 0},
+		{0, 0, 10, 5, 20, 5},
+		{0, 0, 10, 15, 20, 15},
+		{2, 0, 10, 0, 5, 1},
+		{4, 0, 10, 0, 5, 2},
+		{10, 0, 10, 0, 5, 5},
+		{10, 0, 10, 0, 60, 60},
+	}
+	for _, tc := range TestCases {
+		if got := Normalise(tc.in, tc.minVal, tc.maxVal, tc.newMin, tc.newMax); got != tc.expected {
+			t.Errorf("Got %d, but expected %d", got, tc.expected)
 		}
 	}
 }
