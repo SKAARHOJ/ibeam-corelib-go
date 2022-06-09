1588c4bea2d28f02e15561aae295d829bfc318b6
Lukas Bachschwell <lukas@lbsfilm.at>
2022-06-09 16:07:46 +0200

    Update doc uploader
    Signed-off-by: Lukas Bachschwell <lukas@lbsfilm.at>

--- uploadDocs.sh
+++ uploadDocs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -aP --rsync-path="sudo rsync" docs/* rancher@sandbox.skaarhoj.com:/var/lib/docker/volumes/docshost_docs-host-data/_data/ibeam-corelib-go
+rsync -aP -e "ssh -p 50022" docs/* root@localhost:/app/data/ibeam-corelib-go
