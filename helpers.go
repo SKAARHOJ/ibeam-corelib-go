1dcb930de9e9762f55f5f70737b71409e9fa7807
Samuel Z. Jakobsen <78867035+SamuelJakobsen@users.noreply.github.com>
2025-12-17 14:47:50 +0100

    Add GenerateOptionListWithOffset function
    Added GenerateOptionListWithOffset function to create an OptionList with IDs offset by a specified amount.

--- helpers.go
+++ helpers.go
@@ -27,6 +27,18 @@ func GenerateOptionList(options ...string) (optionList *pb.OptionList) {
 	return optionList
 }
 
+// GenerateOptionListWithOffset returns a new OptionList with ascending IDs all offset by a set amount. If offset is 1 first option would have id 1
+func GenerateOptionListWithOffset(offset uint32, options ...string) (optionList *pb.OptionList) {
+	optionList = &pb.OptionList{}
+	for index, option := range options {
+		optionList.Options = append(optionList.Options, &pb.ParameterOption{
+			Id:   uint32(index) + offset,
+			Name: option,
+		})
+	}
+	return optionList
+}
+
 func getIDFromOptionListByElementName(list *pb.OptionList, name string) (uint32, error) {
 	for _, option := range list.Options {
 		if option.Name == name {
